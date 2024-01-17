1981c8108bfa62003955f0b4e4349686df6a517e
Ofek-Carpassi <ofek.carpassi@gmail.com>
2024-01-17 17:27:05 +0200

    handleInputFunctions

--- mainmat.c
+++ mainmat.c
@@ -12,6 +12,12 @@ typedef struct {
 } MatrixMap;
 
 void handleReadMat(MatrixMap* matrixMap, int size);
+void handlePrintMat(MatrixMap* matrixMap, int size);
+void handleAddMat(MatrixMap* matrixMap, int size);
+void handleSubMat(MatrixMap* matrixMap, int size);
+void handleMulMat(MatrixMap* matrixMap, int size);
+void handleMulScalar(MatrixMap* matrixMap, int size);
+void handleTransMat(MatrixMap* matrixMap, int size);
 
 int main()
 {
@@ -47,262 +53,27 @@ int main()
         }
         else if(strcmp(command, "print_mat") == 0)
         {
-            // print_mat <matrix>
-            // Initialize all needed variables, matrixName is the name of the matrix, matrix is the pointer to the matrix
-            char matrixName[6];
-            mat *matrix = NULL;
-
-            // get the matrix (every matrix name is mat_{A-F})
-            scanf("%s", matrixName);
-
-           // find the matrix in the lookup table
-            for(i = 0; i < 6; i++)
-            {
-                if(strcmp(matrixName, matrixMap[i].matrixName) == 0)
-                {
-                    matrix = matrixMap[i].matrix;
-                }
-            }
-
-            // call print_mat(mat*) using the lookup table
-            // if the matrix is not found, print "Undefined matrix name"
-            if(matrix != NULL)
-            {
-                print_mat(matrix);
-            }
-            else
-            {
-                printf("Undefined matrix name\n");
-            }
+            handlePrintMat(matrixMap, 6);
         }
         else if(strcmp(command, "add_mat") == 0)
         {
-            // add_mat <matrix> <matrix> <result matrix>
-            /*
-            Initialize all needed variables:
-            firstMatrix is the name of the first matrix
-            secondMatrix is the name of the second matrix
-            resMatrix is the name of the result matrix
-            firstMatPointer is the pointer to the first matrix
-            secondMatPointer is the pointer to the second matrix
-            resMatPointer is the pointer to the result matrix
-            */
-            char firstMatrix[6], secondMatrix[6], resMatrix[6];
-            mat *firstMatPointer = NULL, *secondMatPointer = NULL, *resMatPointer = NULL;
-
-            // get the matrices (every matrix name is mat_{A-F})
-            scanf("%s %s %s", firstMatrix, secondMatrix, resMatrix);
-
-            // find all the matrices in the lookup table
-            for(i = 0; i < 6; i++)
-            {
-                if(strcmp(firstMatrix, matrixMap[i].matrixName) == 0)
-                {
-                    firstMatPointer = matrixMap[i].matrix;
-                }
-                if(strcmp(secondMatrix, matrixMap[i].matrixName) == 0)
-                {
-                    secondMatPointer = matrixMap[i].matrix;
-                }
-                if(strcmp(resMatrix, matrixMap[i].matrixName) == 0)
-                {
-                    resMatPointer = matrixMap[i].matrix;
-                }
-            }
-
-            // call add_mat(mat*, mat*, mat*) if all the matrices are found in the lookup table
-            if(firstMatPointer != NULL && secondMatPointer != NULL && resMatPointer != NULL)
-            {
-                add_mat(firstMatPointer, secondMatPointer, resMatPointer);
-            }
-            else
-            {
-                printf("Undefined matrix name\n");
-            }
+            handleAddMat(matrixMap, 6);
         }
         else if(strcmp(command, "sub_mat") == 0)
         {
-            // sub_mat <matrix> <matrix> <result matrix>
-            /*
-            Initialize all needed variables:
-            firstMatrix is the name of the first matrix
-            secondMatrix is the name of the second matrix
-            resMatrix is the name of the result matrix
-            firstMatPointer is the pointer to the first matrix
-            secondMatPointer is the pointer to the second matrix
-            resMatPointer is the pointer to the result matrix
-            */
-            char firstMatrix[6], secondMatrix[6], resMatrix[6];
-            mat *firstMatPointer = NULL, *secondMatPointer = NULL, *resMatPointer = NULL;
-
-            // get the matrices names (every matrix name is mat_{A-F})
-            scanf("%s %s %s", firstMatrix, secondMatrix, resMatrix);
-
-            // find all the matrices in the lookup table
-            for(i = 0; i < 6; i++)
-            {
-                if(strcmp(firstMatrix, matrixMap[i].matrixName) == 0)
-                {
-                    firstMatPointer = matrixMap[i].matrix;
-                }
-                if(strcmp(secondMatrix, matrixMap[i].matrixName) == 0)
-                {
-                    secondMatPointer = matrixMap[i].matrix;
-                }
-                if(strcmp(resMatrix, matrixMap[i].matrixName) == 0)
-                {
-                    resMatPointer = matrixMap[i].matrix;
-                }
-            }
-
-            // call sub_mat(mat*, mat*, mat*) if all the matrices are found in the lookup table
-            if(firstMatPointer != NULL && secondMatPointer != NULL && resMatPointer != NULL)
-            {
-                sub_mat(firstMatPointer, secondMatPointer, resMatPointer);
-            }
-            else
-            {
-                printf("Undefined matrix name\n");
-            }
+            handleSubMat(matrixMap, 6);
         }
         else if(strcmp(command, "mul_mat") == 0)
         {
-            // mul_mat <matrix> <matrix> <result matrix>
-            /*
-            Initialize all needed variables:
-            firstMatrix is the name of the first matrix
-            secondMatrix is the name of the second matrix
-            resMatrix is the name of the result matrix
-            firstMatPointer is the pointer to the first matrix
-            secondMatPointer is the pointer to the second matrix
-            resMatPointer is the pointer to the result matrix
-            */
-            char firstMatrix[6], secondMatrix[6], resMatrix[6];
-            mat *firstMatPointer = NULL, *secondMatPointer = NULL, *resMatPointer = NULL;
-
-            // get the matrices names (every matrix name is mat_{A-F})
-            scanf("%s %s %s", firstMatrix, secondMatrix, resMatrix);
-
-            // find all the matrices in the lookup table
-            for(i = 0; i < 6; i++)
-            {
-                if(strcmp(firstMatrix, matrixMap[i].matrixName) == 0)
-                {
-                    firstMatPointer = matrixMap[i].matrix;
-                }
-                if(strcmp(secondMatrix, matrixMap[i].matrixName) == 0)
-                {
-                    secondMatPointer = matrixMap[i].matrix;
-                }
-                if(strcmp(resMatrix, matrixMap[i].matrixName) == 0)
-                {
-                    resMatPointer = matrixMap[i].matrix;
-                }
-            }
-
-            // call mul_mat(mat*, mat*, mat*) if all the matrices are found in the lookup table
-            if(firstMatPointer != NULL && secondMatPointer != NULL && resMatPointer != NULL)
-            {
-                mul_mat(firstMatPointer, secondMatPointer, resMatPointer);
-            }
-            else
-            {
-                printf("Undefined matrix name\n");
-            }
+            handleMulMat(matrixMap, 6);
         }
         else if(strcmp(command, "mul_scalar") == 0)
         {
-            // mul_scalar <matrix> <scalar> <result matrix>
-            /*
-            Initialize all needed variables:
-            matrixName is the name of the matrix
-            scalar is the scalar
-            resMatrix is the name of the result matrix
-            matPointer is the pointer to the matrix
-            resMatPointer is the pointer to the result matrix
-            */
-            char matrixName[6], resMatrix[6];
-            float scalar;
-            int isScalarValid = 1;
-            mat *matPointer = NULL, *resMatPointer = NULL;
-
-            // get the matrix name
-            scanf("%s ", matrixName);
-
-            // make sure the scalar is a real number
-            if(scanf("%f", &scalar) != 1)
-            {
-                printf("Argument is not a real number\n");
-                isScalarValid = 0;
-                continue;
-            }
-
-            if(isScalarValid)
-            {
-                // get the result matrix name
-                scanf("%s", resMatrix);
-
-                // find all the matrices in the lookup table
-                for(i = 0; i < 6; i++)
-                {
-                    if(strcmp(matrixName, matrixMap[i].matrixName) == 0)
-                    {
-                        matPointer = matrixMap[i].matrix;
-                    }
-                    if(strcmp(resMatrix, matrixMap[i].matrixName) == 0)
-                    {
-                        resMatPointer = matrixMap[i].matrix;
-                    }
-                }
-
-                // call mul_scalar(mat*, float, mat*) if all the matrices are found in the lookup table
-                if(matPointer != NULL && resMatPointer != NULL)
-                {
-                    mul_scalar(matPointer, scalar, resMatPointer);
-                }
-                else
-                {
-                    printf("Undefined matrix name\n");
-                }
-            }
+            handleMulScalar(matrixMap, 6);
         }
         else if(strcmp(command, "trans_mat") == 0)
         {
-            // trans_mat <matrix> <result matrix>
-            /*
-            Initialize all needed variables:
-            matrixName is the name of the matrix
-            resMatrix is the name of the result matrix
-            matPointer is the pointer to the matrix
-            resMatPointer is the pointer to the result matrix
-            */
-            char matrixName[6], resMatrix[6];
-            mat *matPointer = NULL, *resMatPointer = NULL;
-
-            // get the matrices names (every matrix name is mat_{A-F})
-            scanf("%s %s", matrixName, resMatrix);
-
-            // find all the matrices in the lookup table
-            for(i = 0; i < 6; i++)
-            {
-                if(strcmp(matrixName, matrixMap[i].matrixName) == 0)
-                {
-                    matPointer = matrixMap[i].matrix;
-                }
-                if(strcmp(resMatrix, matrixMap[i].matrixName) == 0)
-                {
-                    resMatPointer = matrixMap[i].matrix;
-                }
-            }
-
-            if(matPointer != NULL && resMatPointer != NULL)
-            {
-                trans_mat(matPointer, resMatPointer);
-            }
-            else
-            {
-                printf("Undefined matrix name\n");
-            }
+            handleTransMat(matrixMap, 6);
         }
         else if(strcmp(command, "stop") == 0)
         {
@@ -312,6 +83,9 @@ int main()
         else
         {
             printf("Undefined command name\n");
+            // clear the buffer
+            char c;
+            while((c = getchar()) != '\n' && c != EOF);
         }
 
     } while(1);
@@ -365,6 +139,9 @@ void handleReadMat(MatrixMap* matrixMap, int size)
         if(scanf("%f", &numList[i]) != 1)
         {
             printf("Argument is not a real number\n");
+            // clear the buffer
+            char c;
+            while((c = getchar()) != '\n' && c != EOF);
             return;
         }
     }
@@ -373,5 +150,269 @@ void handleReadMat(MatrixMap* matrixMap, int size)
     read_mat(matrix, numList);
 }
 
+void handlePrintMat(MatrixMap* matrixMap, int size)
+{
+    // print_mat <matrix>
+    // Initialize all needed variables, matrixName is the name of the matrix, matrix is the pointer to the matrix
+    char matrixName[6];
+    mat *matrix = NULL;
+
+    // get the matrix (every matrix name is mat_{A-F})
+    scanf("%s", matrixName);
+
+    // find the matrix in the lookup table
+    int i;
+    for(i = 0; i < size; i++)
+    {
+        if(strcmp(matrixName, matrixMap[i].matrixName) == 0)
+        {
+            matrix = matrixMap[i].matrix;
+        }
+    }
+
+    if(matrix == NULL)
+    {
+        printf("Undefined matrix name\n");
+        return;
+    }
+
+    // call print_mat(mat*)
+    print_mat(matrix);
+}
+
+void handleAddMat(MatrixMap* matrixMap, int size)
+{
+    // add_mat <matrix> <matrix> <result matrix>
+    /*
+    Initialize all needed variables:
+    firstMatrix is the name of the first matrix
+    secondMatrix is the name of the second matrix
+    resMatrix is the name of the result matrix
+    firstMatPointer is the pointer to the first matrix
+    secondMatPointer is the pointer to the second matrix
+    resMatPointer is the pointer to the result matrix
+    */
+    char firstMatrix[6], secondMatrix[6], resMatrix[6];
+    mat *firstMatPointer = NULL, *secondMatPointer = NULL, *resMatPointer = NULL;
+
+    // get the matrices (every matrix name is mat_{A-F})
+    scanf("%s %s %s", firstMatrix, secondMatrix, resMatrix);
+
+    // find all the matrices in the lookup table
+    int i;
+    for(i = 0; i < size; i++)
+    {
+        if(strcmp(firstMatrix, matrixMap[i].matrixName) == 0)
+        {
+            firstMatPointer = matrixMap[i].matrix;
+        }
+        if(strcmp(secondMatrix, matrixMap[i].matrixName) == 0)
+        {
+            secondMatPointer = matrixMap[i].matrix;
+        }
+        if(strcmp(resMatrix, matrixMap[i].matrixName) == 0)
+        {
+            resMatPointer = matrixMap[i].matrix;
+        }
+    }
+
+    if(firstMatPointer == NULL || secondMatPointer == NULL || resMatPointer == NULL)
+    {
+        printf("Undefined matrix name\n");
+        return;
+    }
+
+    // call add_mat(mat*, mat*, mat*) if all the matrices are found in the lookup table
+    add_mat(firstMatPointer, secondMatPointer, resMatPointer);
+}
+
+void handleSubMat(MatrixMap* matrixMap, int size)
+{
+    // sub_mat <matrix> <matrix> <result matrix>
+    /*
+    Initialize all needed variables:
+    firstMatrix is the name of the first matrix
+    secondMatrix is the name of the second matrix
+    resMatrix is the name of the result matrix
+    firstMatPointer is the pointer to the first matrix
+    secondMatPointer is the pointer to the second matrix
+    resMatPointer is the pointer to the result matrix
+    */
+    char firstMatrix[6], secondMatrix[6], resMatrix[6];
+    mat *firstMatPointer = NULL, *secondMatPointer = NULL, *resMatPointer = NULL;
+
+    // get the matrices (every matrix name is mat_{A-F})
+    scanf("%s %s %s", firstMatrix, secondMatrix, resMatrix);
+
+    // find all the matrices in the lookup table
+    int i;
+    for(i = 0; i < size; i++)
+    {
+        if(strcmp(firstMatrix, matrixMap[i].matrixName) == 0)
+        {
+            firstMatPointer = matrixMap[i].matrix;
+        }
+        if(strcmp(secondMatrix, matrixMap[i].matrixName) == 0)
+        {
+            secondMatPointer = matrixMap[i].matrix;
+        }
+        if(strcmp(resMatrix, matrixMap[i].matrixName) == 0)
+        {
+            resMatPointer = matrixMap[i].matrix;
+        }
+    }
+
+    if(firstMatPointer == NULL || secondMatPointer == NULL || resMatPointer == NULL)
+    {
+        printf("Undefined matrix name\n");
+        return;
+    }
+
+    // call sub_mat(mat*, mat*, mat*) if all the matrices are found in the lookup table
+    sub_mat(firstMatPointer, secondMatPointer, resMatPointer);
+}
+
+void handleMulMat(MatrixMap* matrixMap, int size)
+{
+    // mul_mat <matrix> <matrix> <result matrix>
+    /*
+    Initialize all needed variables:
+    firstMatrix is the name of the first matrix
+    secondMatrix is the name of the second matrix
+    resMatrix is the name of the result matrix
+    firstMatPointer is the pointer to the first matrix
+    secondMatPointer is the pointer to the second matrix
+    resMatPointer is the pointer to the result matrix
+    */
+    char firstMatrix[6], secondMatrix[6], resMatrix[6];
+    mat *firstMatPointer = NULL, *secondMatPointer = NULL, *resMatPointer = NULL;
+
+    // get the matrices (every matrix name is mat_{A-F})
+    scanf("%s %s %s", firstMatrix, secondMatrix, resMatrix);
+
+    // find all the matrices in the lookup table
+    int i;
+    for(i = 0; i < size; i++)
+    {
+        if(strcmp(firstMatrix, matrixMap[i].matrixName) == 0)
+        {
+            firstMatPointer = matrixMap[i].matrix;
+        }
+        if(strcmp(secondMatrix, matrixMap[i].matrixName) == 0)
+        {
+            secondMatPointer = matrixMap[i].matrix;
+        }
+        if(strcmp(resMatrix, matrixMap[i].matrixName) == 0)
+        {
+            resMatPointer = matrixMap[i].matrix;
+        }
+    }
+
+    if(firstMatPointer == NULL || secondMatPointer == NULL || resMatPointer == NULL)
+    {
+        printf("Undefined matrix name\n");
+        return;
+    }
+
+    // call mul_mat(mat*, mat*, mat*) if all the matrices are found in the lookup table
+    mul_mat(firstMatPointer, secondMatPointer, resMatPointer);
+}
+
+void handleMulScalar(MatrixMap* matrixMap, int size)
+{
+    // mul_scalar <matrix> <scalar> <result matrix>
+    /*
+    Initialize all needed variables:
+    matrixName is the name of the matrix
+    scalar is the scalar
+    resMatrix is the name of the result matrix
+    matPointer is the pointer to the matrix
+    resMatPointer is the pointer to the result matrix
+    */
+    char matrixName[6], resMatrix[6];
+    float scalar;
+    mat *matPointer = NULL, *resMatPointer = NULL;
+
+    // get the matrix name
+    scanf("%s ", matrixName);
+
+    // make sure the scalar is a real number
+    if(scanf("%f", &scalar) != 1)
+    {
+        printf("Argument is not a real number\n");
+        // clear the buffer
+        char c;
+        while((c = getchar()) != '\n' && c != EOF);
+
+        return;
+    }
+
+    // get the result matrix name
+    scanf("%s", resMatrix);
+
+    // find all the matrices in the lookup table
+    int i;
+    for(i = 0; i < size; i++)
+    {
+        if(strcmp(matrixName, matrixMap[i].matrixName) == 0)
+        {
+            matPointer = matrixMap[i].matrix;
+        }
+        if(strcmp(resMatrix, matrixMap[i].matrixName) == 0)
+        {
+            resMatPointer = matrixMap[i].matrix;
+        }
+    }
+    
+    if(matPointer == NULL || resMatPointer == NULL)
+    {
+        printf("Undefined matrix name\n");
+        return;
+    }
+
+    mul_scalar(matPointer, scalar, resMatPointer);
+}
+
+void handleTransMat(MatrixMap* matrixMap, int size)
+{
+    // trans_mat <matrix> <result matrix>
+    /*
+    Initialize all needed variables:
+    matrixName is the name of the matrix
+    resMatrix is the name of the result matrix
+    matPointer is the pointer to the matrix
+    resMatPointer is the pointer to the result matrix
+    */
+    char matrixName[6], resMatrix[6];
+    mat *matPointer = NULL, *resMatPointer = NULL;
+
+    // get the matrix name
+    scanf("%s", matrixName);
+
+    // get the result matrix name
+    scanf("%s", resMatrix);
+
+    // find all the matrices in the lookup table
+    int i;
+    for(i = 0; i < size; i++)
+    {
+        if(strcmp(matrixName, matrixMap[i].matrixName) == 0)
+        {
+            matPointer = matrixMap[i].matrix;
+        }
+        if(strcmp(resMatrix, matrixMap[i].matrixName) == 0)
+        {
+            resMatPointer = matrixMap[i].matrix;
+        }
+    }
+    
+    if(matPointer == NULL || resMatPointer == NULL)
+    {
+        printf("Undefined matrix name\n");
+        return;
+    }
+
+    trans_mat(matPointer, resMatPointer);
+}
 
 //gcc mainmat.c mymat.c -o mainmat
